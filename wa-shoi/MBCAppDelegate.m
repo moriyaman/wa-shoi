29f4bb4b3504f2eded0ba991130dfcfe2896e322
Yuki Moriyama <yuki.moriyaman@gmail.com>
2014-08-25 20:58:28 +0900

    add parse SDK

--- wa-shoi/MBCAppDelegate.m
+++ wa-shoi/MBCAppDelegate.m
@@ -7,12 +7,15 @@
 //
 
 #import "MBCAppDelegate.h"
+#import <Parse/Parse.h>
 
 @implementation MBCAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    [Parse setApplicationId:@"9KDwbeuLlWPeOuT2Zl3rWepD2ZCQ5cHnwkgIFKfN" clientKey:@"caoxn0UqzMpo9s2pmgsfAWvcmZJlEgQeJFHIDU6H"];
+    [PFFacebookUtils initializeFacebook];
     return YES;
 }
 							
@@ -36,11 +39,20 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [FBAppCall handleDidBecomeActiveWithSession:[PFFacebookUtils session]];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    return [FBAppCall handleOpenURL:url
+                  sourceApplication:sourceApplication
+                        withSession:[PFFacebookUtils session]];
 }
 
 @end
