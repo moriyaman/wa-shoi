34da3c6be7394c47d2e3f1838d96488fccd25e73
yhirane <yhirane04ath@gmail.com>
2014-08-27 12:35:56 +0900

    ライブラリ入れた

--- Podfile
+++ Podfile
@@ -1,3 +1,4 @@
 platform :ios, '6.0' #プラットフォームの指定 iOS6.0以上
 xcodeproj './wa-shoi.xcodeproj'
 pod 'Parse-iOS-SDK'
+pod 'EAIntroView', '~> 2.6.0'
---
+++ wa-shoi/IntroViewController.h
@@ -0,0 +1,14 @@
+//
+//  IntroViewController.h
+//  wa-shoi
+//
+//  Created by Yuri Hirane on 2014/08/27.
+//  Copyright (c) 2014年 yuki.moriyama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "EAIntroView.h"
+
+@interface IntroViewController : UIViewController <EAIntroDelegate>
+
+@end
