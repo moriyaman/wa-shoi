d25f55405255ca05b396563711605b156c26ff11
yhirane <yhirane04ath@gmail.com>
2014-09-18 20:29:20 +0900

    デザイン等修正

---
+++ wa-shoi/AccountViewController.m
@@ -0,0 +1,38 @@
+//
+//  AccountViewController.m
+//  wa-shoi
+//
+//  Created by Yuri Hirane on 2014/09/14.
+//  Copyright (c) 2014年 yuki.moriyama. All rights reserved.
+//
+
+#import "AccountViewController.h"
+
+@interface AccountViewController ()
+
+@end
+
+@implementation AccountViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ wa-shoi/TutorialViewController.h
@@ -0,0 +1,13 @@
+//
+//  TutorialViewController.h
+//  wa-shoi
+//
+//  Created by Yuri Hirane on 2014/09/14.
+//  Copyright (c) 2014年 yuki.moriyama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TutorialViewController : UIViewController
+
+@end
